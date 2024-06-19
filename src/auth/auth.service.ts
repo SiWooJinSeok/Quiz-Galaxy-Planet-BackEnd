786234e7c8a5cdb7b816b34f825464cb68a1b82f
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-06-19 15:00:52 +0900

    feat : @aws-sdk/client-cognito-identity-provider install

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { UserInfoEntity } from '../entity/userEntity';
 import { PrismaService } from './../prisma.service';
 import { convertToUserInfoEntity } from './../utils/convertEntity';
+import { CognitoIdentityProviderClient } from '@aws-sdk/client-cognito-identity-provider';
 
 @Injectable()
 export class AuthService {
+  client = new CognitoIdentityProviderClient({ region: 'ap-northeast-2' });
   constructor(private readonly prismaService: PrismaService) {}
 
   async login(): Promise<UserInfoEntity> {
